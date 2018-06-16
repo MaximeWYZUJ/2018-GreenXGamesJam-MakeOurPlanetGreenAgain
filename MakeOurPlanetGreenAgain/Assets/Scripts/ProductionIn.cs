5ccc181a74e5e03fe4bd4ed43cb19833e796f099
MaximeW <wyzmaxou@gmail.com>
2018-06-16 23:45:14 +0200

    import d'assets
    on va commencer à avoir un jeu beau

--- MakeOurPlanetGreenAgain/Assets/Scripts/ProductionIn.cs
+++ MakeOurPlanetGreenAgain/Assets/Scripts/ProductionIn.cs
@@ -13,14 +13,15 @@ public class ProductionIn : GameElement, Production {
 	public void Produce() {
 		GameObject obj = res.GetObject (coo.indexR, coo.indexC);
 		if (obj != null) {
-			if (string.Equals (obj.tag, prefabResource.tag)/* && !string.Equals(obj.tag, "radioactivityWaste") && !string.Equals(obj.tag, "electricity")*/) {
+			if (string.Equals (obj.tag, prefabResource.tag) && !string.Equals(obj.tag, "radioactivityWaste") && !string.Equals(obj.tag, "electricity")) {
 				// La ressource sur le terrain est la meme que la ressource produite par le terrain, donc on stack la nouvelle ressource sur celle deja presente
 				obj.GetComponent<QuantityUnit> ().StackUnit ();
 			} else if (string.Equals (obj.tag, "none")) {
 				// On a une ressource nulle sur le terrain. On la remplace par une ressource produite.
 				obj = InstantiateGameObject (coo.indexR, coo.indexC, prefabResource);
-			}
-			// sinon, la ressource presente est differente donc on ne la remplace pas/ on ne la stack pas
+			} /*else if (string.Equals(obj.tag, "radioactivityWaste") || !string.Equals(obj.tag, "electricity")) {
+				
+			}*/
 		}
 	}
 
